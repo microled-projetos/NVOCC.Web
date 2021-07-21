895ecffb44eedddec0f6cc9b0c48bfae52903684
kleiton-microled <kleiton@microled.com.br>
2021-07-21 17:53:37 -0300

    ajustes TRAKING novos campos

---
+++ NVOCC.Web/Class/Itens.vb
@@ -0,0 +1,9 @@
+﻿Public Class Itens
+    Public Property container_number As String
+    Public Property tare_container As String
+    Public Property gross_weight_decimal As String
+    Public Property volume_m3 As String
+    Public Property seal_number As String
+    Public Property feet As String
+
+End Class
