e148526011eaead13bc06ba01dc80ce7c5d750df
JulianeMicroled <juliane@microled.com.br>
2021-07-29 15:34:54 -0300

    TESTE DE OUTLOOK

---
+++ NVOCC.Web/OUTLOOK.aspx.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Runtime.InteropServices
+Public Class OUTLOOK
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim mapi As SendFileTo.MAPI = New SendFileTo.MAPI()
+        mapi.AddAttachment("c:\temp\file1.txt")
+        mapi.AddAttachment("c:\temp\file2.txt")
+        mapi.AddRecipientTo("person1@somewhere.com")
+        mapi.AddRecipientTo("person2@somewhere.com")
+        mapi.SendMailPopup("testing", "body text")
+
+    End Sub
+
+
+End Class
