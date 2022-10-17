bc6ed34db6493c9e028fce4040e8381214437346
JulianeMicroled <juliane@microled.com.br>
2022-10-17 10:49:10 -0300

    ajustes apos merge  - 17/10/2022 10h50

--- NVOCC.Web/Default.aspx.vb
+++ NVOCC.Web/Default.aspx.vb
@@ -3,7 +3,8 @@ Public Class _Default
     Inherits Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-
+        Dim RotinaDoc As New RotinaDeletaDoc
+        RotinaDoc.DeletaArquivos()
     End Sub
 
 
