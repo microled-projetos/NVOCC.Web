f52fb3cb8b99127353fcb18120d3442f389f3f6c
JulianeMicroled <juliane@microled.com.br>
2022-10-18 11:20:04 -0300

    atualizacao de telas do thiago  - 18/10/2022 11:20

--- NVOCC.Web/DemurrageService.asmx.cs
+++ NVOCC.Web/DemurrageService.asmx.cs
@@ -9123,8 +9123,6 @@ namespace ABAINFRA.Web
 
         [WebMethod]
         public string ExportContaPrevisibilidadeProcesso(string dataI, string dataF, string nota, string filter, string chkConfSim, string chkConfNao)
-<<<<<<< HEAD
-=======
         {
 
             string dtembarque;
@@ -9225,15 +9223,38 @@ namespace ABAINFRA.Web
 
         [WebMethod]
         public string ContaPrevisibilidadeProcesso(string dataI, string dataF, string nota, string filter, string chkConfSim, string chkConfNao)
->>>>>>> devjuliane
         {
+            string diaI = dataI.Substring(8, 2);
+            string mesI = dataI.Substring(5, 2);
+            string anoI = dataI.Substring(0, 4);
 
-            string dtembarque;
-            string dtprevisaochegada;
-            DateTime myDateTime = DateTime.Now;
-            string sqlFormattedDate = myDateTime.AddDays(1).ToString("dd/MM/yyyy");
-            string sqlFormattedDate2 = myDateTime.AddDays(90).ToString("dd/MM/yyyy");
+            string diaF = dataF.Substring(8, 2);
+            string mesF = dataF.Substring(5, 2);
+            string anoF = dataF.Substring(0, 4);
+
+            dataI = diaI + '-' + mesI + '-' + anoI;
+            dataF = diaF + '-' + mesF + '-' + anoF;
+
             string SQL;
+
+            switch (filter)
+            {
+                case "1":
+                    nota = "AND NR_PROCESSO LIKE '" + nota + "%' ";
+                    break;
+                case "2":
+                    nota = "AND NM_CLIENTE LIKE '" + nota + "%' ";
+                    break;
+                case "3":
+                    nota = "AND NM_FORNECEDOR LIKE '" + nota + "%' ";
+                    break;
+                case "4":
+                    nota = "AND NR_BL_MASTER LIKE '" + nota + "%' ";
+                    break;
+                default:
+                    nota = "";
+                    break;
+            }
 
             if (chkConfSim == "1" && chkConfNao == "1")
             {
@@ -9263,81 +9284,20 @@ namespace ABAINFRA.Web
                 }
             }
 
-<<<<<<< HEAD
-            SQL = "SELECT CASE WHEN ISNULL(DOC_CONFERIDO_HOUSE,0) = 0 THEN 'NÃO' ELSE 'SIM' END AS DOC_CONFERIDO_HOUSE, CASE WHEN ISNULL(DOC_CONFERIDO_MASTER,0) = 0 THEN 'NÃO' ELSE 'SIM' END AS DOC_CONFERIDO_MASTER, ISNULL(NR_PROCESSO,'') AS PROCESSO, ISNULL(NR_BL_MASTER,'') MASTER, ISNULL(NR_BL_HOUSE,'') AS HOUSE, ISNULL(TP_SERVICO,'') TPSERVICO, ISNULL(TP_ESTUFAGEM,'') TPESTUFAGEM, ISNULL(TP_PAGAMENTO_HOUSE,'') TPPAGAMENTOHOUSE, ISNULL(TP_PAGAMENTO_MASTER,'') TPPAGAMENTOMASTER, ISNULL(QT_CNTR_20,0) AS CNTR20, ISNULL(QT_CNTR_40,0) AS CNTR40, ISNULL(ORIGEM,'') AS ORIGEM, ISNULL(DESTINO,'') AS DESTINO, DT_EMBARQUE AS DTEMBARQUE, DT_PREVISAO_CHEGADA as DTPREVISAOCHEGADA, ISNULL(NM_CLIENTE,'') AS PARCEIRO, ISNULL(CNEE,'') AS CNEE, ISNULL(INDICADOR,'') AS INDICADOR, ISNULL(AGENTE,'') AS AGENTE,ISNULL(VL_RECEBER,0) AS ARECEBERBR, ISNULL(VL_PAGAR,0) AS APAGARBR, ISNULL(VL_SALDO,0) AS SALDOBR FROM FN_PREVISIBILIDADE_PROCESSO('" + sqlFormattedDate + "','" + sqlFormattedDate2 + "') ";
-=======
             SQL = "SELECT CASE WHEN ISNULL(DOC_CONFERIDO_HOUSE,0) = 0 THEN 'NÃO' ELSE 'SIM' END AS DOC_CONFERIDO_HOUSE, CASE WHEN ISNULL(DOC_CONFERIDO_MASTER,0) = 0 THEN 'NÃO' ELSE 'SIM' END AS DOC_CONFERIDO_MASTER, ISNULL(NR_PROCESSO,'') AS PROCESSO, ISNULL(NR_BL_MASTER,'') MASTER, ISNULL(NR_BL_HOUSE,'') AS HOUSE, ISNULL(TP_SERVICO,'') TPSERVICO, ISNULL(TP_ESTUFAGEM,'') TPESTUFAGEM, ISNULL(TP_PAGAMENTO_HOUSE,'') TPPAGAMENTOHOUSE, ISNULL(TP_PAGAMENTO_MASTER,'') TPPAGAMENTOMASTER, ISNULL(QT_CNTR_20,0) AS CNTR20, ISNULL(QT_CNTR_40,0) AS CNTR40, ISNULL(ORIGEM,'') AS ORIGEM, ISNULL(DESTINO,'') AS DESTINO, FORMAT(DT_EMBARQUE,'dd/MM/yyyy') AS DTEMBARQUE, FORMAT(DT_PREVISAO_CHEGADA,'dd/MM/yyyy') as DTPREVISAOCHEGADA, ISNULL(NM_CLIENTE,'') AS PARCEIRO, ISNULL(CNEE,'') AS CNEE, ISNULL(INDICADOR,'') AS INDICADOR, ISNULL(AGENTE,'') AS AGENTE,CONVERT(DECIMAL(18,2),ISNULL(VL_RECEBER,0)) AS ARECEBERBR, CONVERT(DECIMAL(18,2),ISNULL(VL_PAGAR,0)) AS APAGARBR, ISNULL(TIPO_FATURAMENTO,0) AS TIPO_FATURAMENTO, ISNULL(DIAS_FATURADOS,0) AS DIAS_FATURADOS, CONVERT(DECIMAL(18,2),ISNULL(VL_SALDO,0)) AS SALDOBR, ISNULL(ORIGEM_COMPRA,'') AS ORIGEM_COMPRA, ISNULL(ORIGEM_VENDA, '') AS ORIGEM_VENDA FROM FN_PREVISIBILIDADE_PROCESSO('" + dataI + "','" + dataF + "') ";
->>>>>>> devjuliane
             SQL += "WHERE RIGHT(NR_PROCESSO,2) > 18 ";
             SQL += "" + chkConfSim + "";
             SQL += "" + chkConfNao + "";
+            SQL += "" + nota + "";
             SQL += "ORDER BY NR_PROCESSO ";
             DataTable listTable = new DataTable();
             listTable = DBS.List(SQL);
 
-<<<<<<< HEAD
-            if (listTable != null)
-            {
-                string[] previ = new string[listTable.Rows.Count];
-                for (int i = 0; i < listTable.Rows.Count; i++)
-                {
-                    if (listTable.Rows[i]["DTEMBARQUE"] == null)
-                    {
-                        dtembarque = "";
-                    }
-                    else
-                    {
-                        dtembarque = listTable.Rows[i]["DTEMBARQUE"].ToString();
-                    }
-
-                    if (listTable.Rows[i]["DTPREVISAOCHEGADA"] == null)
-                    {
-                        dtprevisaochegada = "";
-                    }
-                    else
-                    {
-                        dtprevisaochegada = listTable.Rows[i]["DTPREVISAOCHEGADA"].ToString();
-                    }
-                    previ[i] += listTable.Rows[i]["DOC_CONFERIDO_HOUSE"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["DOC_CONFERIDO_MASTER"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["PROCESSO"].ToString() + ";";
-                    previ[i] += fmtTotvs2(listTable.Rows[i]["MASTER"].ToString());
-                    previ[i] += fmtTotvs2(listTable.Rows[i]["HOUSE"].ToString());
-                    previ[i] += listTable.Rows[i]["TPSERVICO"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["TPESTUFAGEM"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["TPPAGAMENTOHOUSE"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["TPPAGAMENTOMASTER"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["CNTR20"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["CNTR40"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["ORIGEM"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["DESTINO"].ToString() + ";";
-                    previ[i] += dtembarque + ";";
-                    previ[i] += dtprevisaochegada + ";";
-                    previ[i] += listTable.Rows[i]["PARCEIRO"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["CNEE"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["INDICADOR"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["AGENTE"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["ARECEBERBR"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["APAGARBR"].ToString() + ";";
-                    previ[i] += listTable.Rows[i]["SALDOBR"].ToString() + ";";
-                }
-                return JsonConvert.SerializeObject(previ);
-            }
-            else
-            {
-                return JsonConvert.SerializeObject(null);
-            }
-        }
-
-        [WebMethod]
-        public string ContaPrevisibilidadeProcesso(string dataI, string dataF, string nota, string filter, string chkConfSim, string chkConfNao)
-=======
             return JsonConvert.SerializeObject(listTable);
         }
 
         [WebMethod]
         public string CSVContaPrevisibilidadeProcesso(string dataI, string dataF, string nota, string filter, string chkConfSim, string chkConfNao)
->>>>>>> devjuliane
         {
             string dtembarque;
             string dtprevisaochegada;
@@ -9402,11 +9362,7 @@ namespace ABAINFRA.Web
                 }
             }
 
-<<<<<<< HEAD
-            SQL = "SELECT CASE WHEN ISNULL(DOC_CONFERIDO_HOUSE,0) = 0 THEN 'NÃO' ELSE 'SIM' END AS DOC_CONFERIDO_HOUSE, CASE WHEN ISNULL(DOC_CONFERIDO_MASTER,0) = 0 THEN 'NÃO' ELSE 'SIM' END AS DOC_CONFERIDO_MASTER, ISNULL(NR_PROCESSO,'') AS PROCESSO, ISNULL(NR_BL_MASTER,'') MASTER, ISNULL(NR_BL_HOUSE,'') AS HOUSE, ISNULL(TP_SERVICO,'') TPSERVICO, ISNULL(TP_ESTUFAGEM,'') TPESTUFAGEM, ISNULL(TP_PAGAMENTO_HOUSE,'') TPPAGAMENTOHOUSE, ISNULL(TP_PAGAMENTO_MASTER,'') TPPAGAMENTOMASTER, ISNULL(QT_CNTR_20,0) AS CNTR20, ISNULL(QT_CNTR_40,0) AS CNTR40, ISNULL(ORIGEM,'') AS ORIGEM, ISNULL(DESTINO,'') AS DESTINO, FORMAT(DT_EMBARQUE,'dd/MM/yyyy') AS DTEMBARQUE, FORMAT(DT_PREVISAO_CHEGADA,'dd/MM/yyyy') as DTPREVISAOCHEGADA, ISNULL(NM_CLIENTE,'') AS PARCEIRO, ISNULL(CNEE,'') AS CNEE, ISNULL(INDICADOR,'') AS INDICADOR, ISNULL(AGENTE,'') AS AGENTE,CONVERT(DECIMAL(18,2),ISNULL(VL_RECEBER,0)) AS ARECEBERBR, CONVERT(DECIMAL(18,2),ISNULL(VL_PAGAR,0)) AS APAGARBR, ISNULL(TIPO_FATURAMENTO,0) AS TIPO_FATURAMENTO, ISNULL(DIAS_FATURADOS,0) AS DIAS_FATURADOS, CONVERT(DECIMAL(18,2),ISNULL(VL_SALDO,0)) AS SALDOBR, ISNULL(ORIGEM_COMPRA,'') AS ORIGEM_COMPRA, ISNULL(ORIGEM_VENDA, '') AS ORIGEM_VENDA FROM FN_PREVISIBILIDADE_PROCESSO('" + dataI + "','" + dataF + "') ";
-=======
             SQL = "SELECT CASE WHEN ISNULL(DOC_CONFERIDO_HOUSE,0) = 0 THEN 'NÃO' ELSE 'SIM' END AS DOC_CONFERIDO_HOUSE, CASE WHEN ISNULL(DOC_CONFERIDO_MASTER,0) = 0 THEN 'NÃO' ELSE 'SIM' END AS DOC_CONFERIDO_MASTER, ISNULL(NR_PROCESSO,'') AS PROCESSO, ISNULL(NR_BL_MASTER,'') MASTER, ISNULL(NR_BL_HOUSE,'') AS HOUSE, ISNULL(TP_SERVICO,'') TPSERVICO, ISNULL(TP_ESTUFAGEM,'') TPESTUFAGEM, ISNULL(TP_PAGAMENTO_HOUSE,'') TPPAGAMENTOHOUSE, ISNULL(TP_PAGAMENTO_MASTER,'') TPPAGAMENTOMASTER, ISNULL(QT_CNTR_20,0) AS CNTR20, ISNULL(QT_CNTR_40,0) AS CNTR40, ISNULL(ORIGEM,'') AS ORIGEM, ISNULL(DESTINO,'') AS DESTINO, ISNULL(FORMAT(DT_EMBARQUE,'dd/MM/yyyy'),'') AS DTEMBARQUE, ISNULL(FORMAT(DT_PREVISAO_CHEGADA,'dd/MM/yyyy'),'') as DTPREVISAOCHEGADA, ISNULL(NM_CLIENTE,'') AS PARCEIRO, ISNULL(CNEE,'') AS CNEE, ISNULL(INDICADOR,'') AS INDICADOR, ISNULL(AGENTE,'') AS AGENTE,CONVERT(DECIMAL(18,2),ISNULL(VL_RECEBER,0)) AS ARECEBERBR, CONVERT(DECIMAL(18,2),ISNULL(VL_PAGAR,0)) AS APAGARBR, ISNULL(TIPO_FATURAMENTO,0) AS TIPO_FATURAMENTO, ISNULL(DIAS_FATURADOS,0) AS DIAS_FATURADOS, CONVERT(DECIMAL(18,2),ISNULL(VL_SALDO,0)) AS SALDOBR, ISNULL(ORIGEM_COMPRA,'') AS ORIGEM_COMPRA, ISNULL(ORIGEM_VENDA, '') AS ORIGEM_VENDA FROM FN_PREVISIBILIDADE_PROCESSO('" + dataI + "','" + dataF + "') ";
->>>>>>> devjuliane
             SQL += "WHERE RIGHT(NR_PROCESSO,2) > 18 ";
             SQL += "" + chkConfSim + "";
             SQL += "" + chkConfNao + "";
@@ -9415,87 +9371,6 @@ namespace ABAINFRA.Web
             DataTable listTable = new DataTable();
             listTable = DBS.List(SQL);
 
-<<<<<<< HEAD
-            return JsonConvert.SerializeObject(listTable);
-        }
-
-        [WebMethod]
-        public string CSVContaPrevisibilidadeProcesso(string dataI, string dataF, string nota, string filter, string chkConfSim, string chkConfNao)
-        {
-            string dtembarque;
-            string dtprevisaochegada;
-
-            string diaI = dataI.Substring(8, 2);
-            string mesI = dataI.Substring(5, 2);
-            string anoI = dataI.Substring(0, 4);
-
-            string diaF = dataF.Substring(8, 2);
-            string mesF = dataF.Substring(5, 2);
-            string anoF = dataF.Substring(0, 4);
-
-            dataI = diaI + '-' + mesI + '-' + anoI;
-            dataF = diaF + '-' + mesF + '-' + anoF;
-
-            string SQL;
-
-            switch (filter)
-            {
-                case "1":
-                    nota = "AND NR_PROCESSO LIKE '" + nota + "%' ";
-                    break;
-                case "2":
-                    nota = "AND NM_CLIENTE LIKE '" + nota + "%' ";
-                    break;
-                case "3":
-                    nota = "AND NM_FORNECEDOR LIKE '" + nota + "%' ";
-                    break;
-                case "4":
-                    nota = "AND NR_BL_MASTER LIKE '" + nota + "%' ";
-                    break;
-                default:
-                    nota = "";
-                    break;
-            }
-
-            if (chkConfSim == "1" && chkConfNao == "1")
-            {
-                chkConfSim = "";
-                chkConfNao = "";
-            }
-            else
-            {
-                switch (chkConfNao)
-                {
-                    case "1":
-                        chkConfNao = " AND (ISNULL(DOC_CONFERIDO_HOUSE,0)=0 AND ISNULL(DOC_CONFERIDO_MASTER,0)=0) ";
-                        break;
-                    default:
-                        chkConfNao = "";
-                        break;
-                }
-
-                switch (chkConfSim)
-                {
-                    case "1":
-                        chkConfSim = " AND (DOC_CONFERIDO_HOUSE != 0 OR DOC_CONFERIDO_MASTER != 0) ";
-                        break;
-                    default:
-                        chkConfSim = "";
-                        break;
-                }
-            }
-
-            SQL = "SELECT CASE WHEN ISNULL(DOC_CONFERIDO_HOUSE,0) = 0 THEN 'NÃO' ELSE 'SIM' END AS DOC_CONFERIDO_HOUSE, CASE WHEN ISNULL(DOC_CONFERIDO_MASTER,0) = 0 THEN 'NÃO' ELSE 'SIM' END AS DOC_CONFERIDO_MASTER, ISNULL(NR_PROCESSO,'') AS PROCESSO, ISNULL(NR_BL_MASTER,'') MASTER, ISNULL(NR_BL_HOUSE,'') AS HOUSE, ISNULL(TP_SERVICO,'') TPSERVICO, ISNULL(TP_ESTUFAGEM,'') TPESTUFAGEM, ISNULL(TP_PAGAMENTO_HOUSE,'') TPPAGAMENTOHOUSE, ISNULL(TP_PAGAMENTO_MASTER,'') TPPAGAMENTOMASTER, ISNULL(QT_CNTR_20,0) AS CNTR20, ISNULL(QT_CNTR_40,0) AS CNTR40, ISNULL(ORIGEM,'') AS ORIGEM, ISNULL(DESTINO,'') AS DESTINO, FORMAT(DT_EMBARQUE,'dd/MM/yyyy') AS DTEMBARQUE, FORMAT(DT_PREVISAO_CHEGADA,'dd/MM/yyyy') as DTPREVISAOCHEGADA, ISNULL(NM_CLIENTE,'') AS PARCEIRO, ISNULL(CNEE,'') AS CNEE, ISNULL(INDICADOR,'') AS INDICADOR, ISNULL(AGENTE,'') AS AGENTE,CONVERT(DECIMAL(18,2),ISNULL(VL_RECEBER,0)) AS ARECEBERBR, CONVERT(DECIMAL(18,2),ISNULL(VL_PAGAR,0)) AS APAGARBR, ISNULL(TIPO_FATURAMENTO,0) AS TIPO_FATURAMENTO, ISNULL(DIAS_FATURADOS,0) AS DIAS_FATURADOS, CONVERT(DECIMAL(18,2),ISNULL(VL_SALDO,0)) AS SALDOBR, ISNULL(ORIGEM_COMPRA,'') AS ORIGEM_COMPRA, ISNULL(ORIGEM_VENDA, '') AS ORIGEM_VENDA FROM FN_PREVISIBILIDADE_PROCESSO('" + dataI + "','" + dataF + "') ";
-            SQL += "WHERE RIGHT(NR_PROCESSO,2) > 18 ";
-            SQL += "" + chkConfSim + "";
-            SQL += "" + chkConfNao + "";
-            SQL += "" + nota + "";
-            SQL += "ORDER BY NR_PROCESSO ";
-            DataTable listTable = new DataTable();
-            listTable = DBS.List(SQL);
-
-=======
->>>>>>> devjuliane
             if (listTable != null)
             {
                 string[] previ = new string[listTable.Rows.Count];
