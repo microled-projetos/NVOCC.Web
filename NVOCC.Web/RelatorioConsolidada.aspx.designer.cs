b9d0e6e0ad81b42ecfea6112829ba0adb898c2cf
JulianeMicroled <juliane@microled.com.br>
2023-06-23 17:59:58 -0300

    chamado 3853: atualização telas do thiago

--- NVOCC.Web/RelatorioConsolidada.aspx.designer.cs
+++ NVOCC.Web/RelatorioConsolidada.aspx.designer.cs
@@ -13,5 +13,59 @@ namespace ABAINFRA.Web
 
 	public partial class RelatorioConsolidada
 	{
+
+		/// <summary>
+		/// Controle chkLTLS.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.CheckBox chkLTLS;
+
+		/// <summary>
+		/// Controle chkLTLN.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.CheckBox chkLTLN;
+
+		/// <summary>
+		/// Controle chkDTAHUBS.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.CheckBox chkDTAHUBS;
+
+		/// <summary>
+		/// Controle chkDTAHUBN.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.CheckBox chkDTAHUBN;
+
+		/// <summary>
+		/// Controle chkTranspDedicS.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.CheckBox chkTranspDedicS;
+
+		/// <summary>
+		/// Controle chkTranspDedicN.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.CheckBox chkTranspDedicN;
 	}
 }
